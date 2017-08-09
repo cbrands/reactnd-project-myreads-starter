17e44abb61b15cb80b3ba1670e033106e8950e0c
charles-ordina <charles.brands@ordina.nl>
2017-08-09 12:56:14 +0200

    extracted header to a separate object

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Header from './Header'
+
 
 class ListBooks extends Component {
     static propTypes = {
@@ -10,9 +12,7 @@ class ListBooks extends Component {
     render() {
         return (
         <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+            <Header />
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
