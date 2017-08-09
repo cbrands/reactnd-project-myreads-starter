ea4b8458614a24b4396a51ca388d59e07f1ad8d2
charles-ordina <charles.brands@ordina.nl>
2017-08-09 20:07:26 +0200

    SelfChangeMenu handle menu change

--- src/ShelfChangeMenu.js
+++ src/ShelfChangeMenu.js
@@ -6,16 +6,27 @@ class ShelfChangeMenu extends Component {
         shelf: PropTypes.string.isRequired
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {value: this.props.shelf};
+        this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+      alert('changing to: ' + event.target.value);
+  }
+
     render() {
         return (
             <div className="book-shelf-changer">
-                <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
             </div>
         )
     }
